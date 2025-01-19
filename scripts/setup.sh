acd8adc20e77d1a7c8fba9e1f9efd357957a0b02
System233 <20336040+System233@users.noreply.github.com>
2025-01-19 14:28:02 +0800

    fix: 复制entries资源到share

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,9 +3,8 @@ source $(dirname $0)/env.sh
 mkdir -p $BIN_DIR
 cp -af $LL_KILLER_EXEC $LL_ENTRYPOINT $PREFIX
 ln -sf $LL_ENTRYPOINT_ROOT $LL_ENTRYPOINT_BIN
-mkdir -p $PREFIX/share
-cp -arf $PREFIX/opt/apps/*/entries/ $PREFIX/share || true
-mv $PREFIX/usr/share $PREFIX/share
+mv $PREFIX/usr/share $PREFIX/share || mkdir -p $PREFIX/share
 mkdir -p $PREFIX/usr/share
+cp -arf $PREFIX/opt/apps/*/entries/* $PREFIX/share || true
 find $PREFIX/share -xtype l -exec "$SCRIPT_DIR/relink.sh" "{}" \;
 find $PREFIX/share/applications -name "*.desktop" -exec "$SCRIPT_DIR/setup-desktop.sh" "{}" \; || true
