cb491428245001fdc56e04bcb6a2722a571f1dd7
System233 <20336040+System233@users.noreply.github.com>
2025-01-20 14:11:47 +0800

    删除构建日志中的进度条

--- scripts/build-and-check.sh
+++ scripts/build-and-check.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 ROOT_DIR=$(dirname $(readlink -f $0))
-ll-builder build
+ll-builder build | grep --line-buffered -Pv "={3,}"
 echo Runing ldd-check...
 ll-builder run --exec "entrypoint.sh $ROOT_DIR/ldd-check.sh" | tr -d '\r' | tee ldd-check.log
 
 $ROOT_DIR/ll-killer ldd-check >ldd-check.log
 $ROOT_DIR/ll-killer ldd-search ldd-check.log ldd-found.log ldd-missing.log
 
-FOUND=$(cat ldd-found.log)
+FOUND=$(cat ldd-found.log || true)
 if [ -n "$FOUND" ]; then
     echo Found libraries:
     cat ldd-found.log
-    ll-builder build
+    ll-builder build | grep --line-buffered -Pv "={3,}"
     $ROOT_DIR/ll-killer ldd-check >ldd-check.log
 fi
