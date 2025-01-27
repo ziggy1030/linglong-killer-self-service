24ea98c47ee45ddef59e33a00ca61576e6e86859
System233 <20336040+System233@users.noreply.github.com>
2025-01-28 00:58:10 +0800

    避免搜索到多个匹配依赖库时产生的冲突

--- scripts/ldd-search.sh
+++ scripts/ldd-search.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 FOUND=${2:-/dev/stdout}
 MISSING=${3:-/dev/stderr}
-SEARCHED=$(apt-file find -f ${1:--} | grep '^lib' | sort -u)
+INPUT=${1:--}
+SEARCHED=$(cat $INPUT | xargs -P$(nproc) -I{} sh -c 'apt-file find -x "{}$"| grep -P "^lib|/usr/lib/x86_64-linux-gnu/" | sort -urk2 | head -n1')
 grep -Fvf <(echo "$SEARCHED" | awk -F/ '{print $NF}') ldd-check.log >$MISSING
 echo "$SEARCHED" | cut -d: -f1 | sort -u >$FOUND
