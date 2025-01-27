26b5e2262e16bca6cb64833383a3fd11d6831737
System233 <20336040+System233@users.noreply.github.com>
2025-01-28 00:57:18 +0800

    支持排除非默认路径下的依赖库

--- scripts/ldd-check.sh
+++ scripts/ldd-check.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-find /opt /usr /lib /bin '(' -name "*.so" -or -executable ')' | xargs ldd 2>/dev/null | grep -F "=> not found" | sort -u | grep -oP '^\s*\K\S+'
+TMP_DIR=$(mktemp -d ll-killer.XXXXXX -p /tmp)
+TMP_FILE="$TMP_DIR/soname.list"
+DIR_LIST="/opt /usr /lib /bin"
+find $DIR_LIST -name "*.so*" | xargs -n1 basename | sort -u >$TMP_FILE
+find $DIR_LIST '(' -name "*.so" -or -executable ')' | xargs ldd 2>/dev/null | grep -F "=> not found" | sort -u | grep -oP '^\s*\K\S+' | grep -vFxf "$TMP_FILE"
+rm -rf $TMP_DIR
